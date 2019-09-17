e9954bb016f6678c48206406350ed62d865eb83d
Roben Kleene <contact@robenkleene.com>
2019-09-17 13:59:33 -0400

    First commit

---
+++ Lottie/Cocoa/LottieDemoMac/LottieDemoMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LottieDemoMac
+//
+//  Created by Roben Kleene on 9/17/19.
+//  Copyright © 2019 Roben Kleene. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
