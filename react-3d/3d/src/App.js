dbafd426abb36026d5a5ddf092445393113025dd
Roben Kleene <contact@robenkleene.com>
2020-08-06 12:52:30 -0400

    Work on tutorial

--- react-3d/3d/src/App.js
+++ react-3d/3d/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Canvas } from "react-three-fiber";
 
 // import './App.css';
 
+function Cube() {
+  return (
+    <mesh>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+    </mesh>
+  )
+}
+
+function Scene() {
+  return (
+    <>
+      <ambientLight />:
+      <pointLight position={[-1, 2, 4]} />
+    </ >
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Canvas>
+      <Scene />
+    </Canvas>
   );
 }
 
