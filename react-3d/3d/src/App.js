e2c07f6fb2562fd34cab7e5de86918e116cdfcdd
Roben Kleene <contact@robenkleene.com>
2020-08-10 15:45:13 -0400

    Update tutorial

--- react-3d/3d/src/App.js
+++ react-3d/3d/src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 
 function Cube() {
   return (
-    <mesh position={[0, 0, 0]}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+    <mesh rotation={[10, 10, 0]} position={[0, 0, 0]}>
+      <boxBufferGeometry attach="geometry" args={[2, 2, 1]} />
       <meshStandardMaterial attach="material" color="pink" />
     </mesh>
   );
@@ -16,7 +16,7 @@ function Scene() {
   return (
     <>
       <ambientLight />:
-      <pointLight position={[-1, 2, 4]} />
+      <pointLight intensity={0.3} position={[-1, 2, 4]} />
       <Cube />
     </>
   );
