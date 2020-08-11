9f0aeda3a16942a5a50f3becd9bbb7d544aa22f2
Roben Kleene <contact@robenkleene.com>
2020-08-11 18:01:03 -0400

    Add view

---
+++ swiftui/Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Roben Kleene on 8/11/20.
+//  Copyright © 2020 Roben Kleene. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
