71dc63f0e66471fd4948109cc273a5456c61c23c
Roben Kleene <contact@robenkleene.com>
2020-08-12 19:20:57 -0400

    Add MapView

---
+++ swiftui/Landmarks/Landmarks/MapView.swift
@@ -0,0 +1,22 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Roben Kleene on 8/12/20.
+//  Copyright © 2020 Roben Kleene. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
