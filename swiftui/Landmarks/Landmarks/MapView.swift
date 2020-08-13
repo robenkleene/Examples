4d787152e6045b3ff8ba60cba257ac15f5ace71c
Roben Kleene <contact@robenkleene.com>
2020-08-13 13:23:30 -0400

    Flesh out map view

--- swiftui/Landmarks/Landmarks/MapView.swift
+++ swiftui/Landmarks/Landmarks/MapView.swift
@@ -10,8 +10,16 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    func makeUIView(context: Context) -> some UIView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
     }
 }
 
