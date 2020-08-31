434545877bacb1d23429893ff09eb102e4543f1a
Roben Kleene <contact@robenkleene.com>
2020-08-31 07:39:27 -0400

    Add plane

--- react-3d/3d/src/App.js
+++ react-3d/3d/src/App.js
@@ -48,6 +48,15 @@ function Cube(props) {
   );
 }
 
+function Plane() {
+  return (
+    <mesh>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshStandardMaterial attach="material" color="blue" />
+    </mesh>
+  )
+}
+
 function Scene() {
   const {
     camera,
@@ -57,9 +66,10 @@ function Scene() {
   return (
     <>
       <ambientLight />:
-      <pointLight intensity={0.3} position={[-1, 2, 0]} />
+      <pointLight intensity={0.3} position={[0, 0, 3]} />
       <Cube rotation={[10, 10, 0]} position={[0, 0, 0]} />
       <Cube rotation={[10, 20, 0]} position={[2, 2, 0]} />
+      <Plane />
       <orbitControls args={[camera, domElement]} />
     </>
   );
