b21f7dbe8a4af7e49364bddd876bf1690d4e8f94
Roben Kleene <contact@robenkleene.com>
2020-09-07 07:26:34 -0400

    Re-do Button with generate react

--- react/create-react-app-snap/src/App.js
+++ react/create-react-app-snap/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Button from "components/Button"
+import Button from "./components/Button/Button.js"
 
 function App() {
   return (
---
+++ react/create-react-app-snap/src/components/Button/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import './Button.css';
+
+const Button = () => (
+  <div className="Button">
+    Button Component
+  </div>
+);
+
+Button.propTypes = {};
+
+Button.defaultProps = {};
+
+export default Button;
