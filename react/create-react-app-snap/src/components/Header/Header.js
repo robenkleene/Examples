830346a586b20ac4de3b03624b32d490c9272091
Roben Kleene <contact@robenkleene.com>
2020-09-08 07:26:35 -0400

    Remove defaults and add header

---
+++ react/create-react-app-snap/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Header.css';
+
+const Header = () => (
+  <h1 className="Header">
+    Header
+  </h1>
+);
+
+Header.propTypes = {};
+
+Header.defaultProps = {};
+
+export default Header;
