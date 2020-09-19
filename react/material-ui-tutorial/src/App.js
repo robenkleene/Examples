d68ab177dcdcd466216509cd2e88d7310175d641
Roben Kleene <contact@robenkleene.com>
2020-09-19 07:06:38 -0400

    Add first button

--- react/material-ui-tutorial/src/App.js
+++ react/material-ui-tutorial/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
+      <Button>This is our first button</Button>
     </div>
   );
 }
