8fa5efc95a50dd918ce2139032ae3f496fe89874
Roben Kleene <contact@robenkleene.com>
2020-09-21 20:33:02 -0400

    Work on matrix

--- react/react-3d/3d/src/App.js
+++ react/react-3d/3d/src/App.js
@@ -4,16 +4,31 @@ import { OrbitControls } from "drei";
 import { Canvas, useFrame } from "react-three-fiber";
 import "./App.css";
 
-function Boxes() {
+const tempObject = new THREE.Object3D();
 
+function Boxes() {
   const ref = useRef();
 
+  useFrame(() => {
+    let i = 0;
+    for (let x = 0; x < 10; x++) {
+      for (let y = 0; y < 10; y++) {
+        for (let z = 0; z < 10; z++) {
+          const id = i++;
+          tempObject.position.set(5 - x, 5 - y, 5 - z);
+          tempObject.updateMatrix();
+          ref.current.setMatrix(id, tempObject.matrix);
+        }
+      }
+    }
+  });
+
   return (
-    <instancedMesh ref={ref}>
+    <instancedMesh ref={ref} args={[null, null, 1000]}>
       <boxBufferGeometry attach="geometry" args={[0.7, 0.7, 0.7]} />
       <meshPhongMaterial attach="material" color="teal" />
     </instancedMesh>
-  )
+  );
 }
 
 function Scene() {
