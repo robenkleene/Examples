d2122a78467b29749b703d7df95455651f073f20
Roben Kleene <contact@robenkleene.com>
2020-10-07 07:03:56 -0400

    Add shaders

--- effects/planet-shader-recreate/script.js
+++ effects/planet-shader-recreate/script.js
@@ -1,3 +1,29 @@
+
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+var colormap = new THREE.TextureLoader().load(
+  "https://raw.githubusercontent.com/pizza3/asset/master/color.png"
+);
+var color = new THREE.TextureLoader().load(
+  "https://raw.githubusercontent.com/pizza3/asset/master/noise2.jpg"
+);
+var noi = new THREE.TextureLoader().load(
+  "https://raw.githubusercontent.com/pizza3/asset/master/fluid.jpg"
+);
+var uniforms = {
+  time: {
+    type: "f",
+    value: 10.0,
+  },
+  resolution: {
+    value: new THREE.Vector2(width, height),
+  },
+  color: { type: "f", value: color },
+  colormap: { type: "f", value: colormap },
+  noiseTex: { type: "f", value: noi },
+};
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
   75,
@@ -16,14 +42,25 @@ camera.position.z = 5;
 
 // Model
 var geometry = new THREE.SphereGeometry(2, 32, 32);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var material = new THREE.ShaderMaterial({
+  uniforms: uniforms,
+  transparent: true,
+  vertexShader: document.getElementById("vertexShader").textContent,
+  fragmentShader: document.getElementById("fragmentShader").textContent,
+});
 var sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
 function animate() {
   requestAnimationFrame(animate);
-  sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.01;
+  material.uniforms.time.value = delta * 0.6;
   renderer.render(scene, camera);
 }
 animate();
+
+function handleResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener("resize", handleResize, false);
