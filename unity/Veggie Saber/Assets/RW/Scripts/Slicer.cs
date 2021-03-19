14752462fc7aacc1485158d7618d2ba3efa29fdf
Roben Kleene <contact@robenkleene.com>
2021-03-18 20:53:35 -0400

    Import RW assets

---
+++ unity/Veggie Saber/Assets/RW/Scripts/Slicer.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * Copyright (c) 2019 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
+ * distribute, sublicense, create a derivative work, and/or sell copies of the 
+ * Software in any work that is designed, intended, or marketed for pedagogical or 
+ * instructional purposes related to programming, coding, application development, 
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * or sale is expressly withheld.
+ *    
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slicer : MonoBehaviour
+{
+    public GameObject gameManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // FILL IN
+    }
+
+    // Get a cutting plane from the rotation/position of the saber
+    private Plane GetPlane(GameObject go)
+    {
+        Plane rv = new Plane();
+
+        // FILL IN
+
+        return rv;
+    }
+
+    // Clone a Mesh "half"
+    private Mesh CloneMesh(Plane p, Mesh oMesh, bool halve)
+    {
+        Mesh cMesh = new Mesh();
+
+        // FILL IN
+
+        return cMesh;
+    }
+
+    // Configure the GameObject
+    private GameObject MakeHalf(GameObject go, bool isLeft)
+    {
+        // 1.
+        float sign = isLeft ? -1 : 1;
+        GameObject half = Instantiate(go);
+
+        // FILL IN
+
+        return half;
+    }
+
+    // Make two GameObjects with "halves" of the original
+    private void SplitMesh(GameObject go)
+    {
+        // FILL IN
+    }
+}
