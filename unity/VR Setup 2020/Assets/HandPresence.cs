03a5e58972d8ef2fd77daa8878009e487edbddd3
Roben Kleene <contact@robenkleene.com>
2021-04-06 15:35:38 -0400

    Make initialization retry

--- unity/VR Setup 2020/Assets/HandPresence.cs
+++ unity/VR Setup 2020/Assets/HandPresence.cs
@@ -15,21 +15,7 @@ public class HandPresence : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        List<InputDevice> devices = new List<InputDevice>();
-
-        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
-
-        foreach (var item in devices)
-        {
-            Debug.Log(item.name + item.characteristics);
-        }
-
-        if (devices.Count > 0) {
-            targetDevice = devices[0];
-        }
-
-        spawnedHandModel = Instantiate(handModelPrefab, transform);
-        handAnimator = spawnedHandModel.GetComponent<Animator>();
+        TryInitialize();
     }
 
     void UpdateHandAnimation()
@@ -53,24 +39,51 @@ public class HandPresence : MonoBehaviour
         }
     }
 
+
+    void TryInitialize()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if (devices.Count > 0) {
+            targetDevice = devices[0];
+        }
+
+        spawnedHandModel = Instantiate(handModelPrefab, transform);
+        handAnimator = spawnedHandModel.GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        spawnedHandModel.SetActive(true);
-        UpdateHandAnimation();
-
-        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
-        if (primaryButtonValue) {
-            Debug.Log("Pressing primary button");
+        if (!targetDevice.isValid)
+        {
+            TryInitialize();
         }
-        targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
-        if (triggerValue > 0.1f) {
-            Debug.Log("Trigger pressed " + triggerValue);
-        }
+        else
+        {
+            spawnedHandModel.SetActive(true);
+            UpdateHandAnimation();
 
-        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
-        if (primary2DAxisValue != Vector2.zero) {
-            Debug.Log("Primary Touchpad " + primary2DAxisValue);
+            targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+            if (primaryButtonValue) {
+                Debug.Log("Pressing primary button");
+            }
+            targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+            if (triggerValue > 0.1f) {
+                Debug.Log("Trigger pressed " + triggerValue);
+            }
+
+            targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
+            if (primary2DAxisValue != Vector2.zero) {
+                Debug.Log("Primary Touchpad " + primary2DAxisValue);
+            }
         }
     }
 }
