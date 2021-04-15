d363d1d629594dfeede32632fa6d0915731c1e87
Roben Kleene <contact@robenkleene.com>
2021-04-14 20:43:29 -0400

    Add continuous movement

---
+++ unity/VR Setup 2020/Assets/ContinuousMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ContinuousMovement : MonoBehaviour
+{
+    public XRNode inputSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
