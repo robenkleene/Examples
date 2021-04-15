b341ec1f0bdd536962c174a44308aa3006f6e625
Roben Kleene <contact@robenkleene.com>
2021-04-14 20:59:31 -0400

    Update script

--- unity/VR Setup 2020/Assets/ContinuousMovement.cs
+++ unity/VR Setup 2020/Assets/ContinuousMovement.cs
@@ -6,17 +6,29 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class ContinuousMovement : MonoBehaviour
 {
+    public float speed = 1;
     public XRNode inputSource;
+    private Vector2 inputAxis;
+    private CharacterController character;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        character = GetComponent<CharacterController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(inputAxis.x, 0, inputAxis.y);
+
+        character.Move(direction * Time.fixedDeltaTime * speed);
     }
 }
